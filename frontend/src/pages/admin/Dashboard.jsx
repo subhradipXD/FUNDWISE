f82a803b1c8b2fa509e234c351f5ac6a23287041
Subhradip <subhradipdas6969@gmail.com>
2024-05-19 02:41:33 +0530

    dashboard authentication

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { UserContext } from "../../Context/ContextProvider";
 import { StyleDiv } from "./dasboardComponents/dashboard-css";
 import DashboardSidebar from "./dasboardComponents/Dashboard-Sidebar";
 import DashboardNavbar from "./dasboardComponents/Dashboard-Navbar";
 import DashboardMain from "./dasboardComponents/Dashboard-Main";
+
 function Dashboard() {
+  const navigate = useNavigate();
+  const [cookies] = useCookies(["token"]);
+  const { user } = useContext(UserContext);
+
+  // Check for the token and user role
+  if (!cookies.token || (user && user.role !== "Admin")) {
+    navigate("/");
+  }
+
   return (
     <StyleDiv>
       <div className="body">
